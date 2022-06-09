6186c760d0a7d5f09ae57078b86699c14674ce98
77r77ung <2625k@naver.com>
2022-06-10 02:51:55 +0900

    2022.06.09~2022.06.10

--- level1/1_7.py
+++ level1/1_7.py
@@ -14,19 +14,22 @@
 '''
 
 def solution(num):
-
     i = 0
-    while num == 1:
+
+    while True:
+        if num == 1:
+            break
+        elif i >= 500:
+            return -1
+            break
+        elif num % 2 == 0:
+            num = num / 2
+        else:
+            num = num * 3 + 1
         i += 1
 
-        if i >= 500:
-            print('-1')
-        else:
-            if num % 2 == 0:
-                num = num / 2
-            else:
-                num = num * 3 + 1
     return i
 
-
-print(solution(6))
+# print(solution(6))
+# print(solution(16))
+# print(solution(626331))
